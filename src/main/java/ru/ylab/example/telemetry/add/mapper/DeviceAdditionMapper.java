90c9b1efda84c6cf14f6489f85e763dbe23af638
Klepa0034 <sorokinga04@mail.ru>
2026-02-15 21:51:45 +0400

    [T-006] Написал Controller

--- src/main/java/ru/ylab/example/telemetry/add/mapper/DeviceAdditionMapper.java
+++ src/main/java/ru/ylab/example/telemetry/add/mapper/DeviceAdditionMapper.java
@@ -4,7 +4,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
-import ru.ylab.example.telemetry.add.dto.DeviceAdditionDto;
+import ru.ylab.example.telemetry.add.model.DeviceAdditionDto;
+import ru.ylab.example.telemetry.add.model.DeviceAdditionRequest;
 import ru.ylab.example.telemetry.share.Device;
 
 @Mapper( unmappedTargetPolicy = ReportingPolicy.ERROR,
@@ -12,4 +13,5 @@ import ru.ylab.example.telemetry.share.Device;
 public interface DeviceAdditionMapper {
     @Mapping(target = "id", ignore = true)
     Device map(DeviceAdditionDto deviceAddition);
+    DeviceAdditionDto map(DeviceAdditionRequest request);
 }
--- src/main/java/ru/ylab/example/telemetry/add/service/impl/DeviceAdditionServiceImpl.java
+++ src/main/java/ru/ylab/example/telemetry/add/service/impl/DeviceAdditionServiceImpl.java
@@ -2,7 +2,7 @@ package ru.ylab.example.telemetry.add.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.ylab.example.telemetry.add.dto.DeviceAdditionDto;
+import ru.ylab.example.telemetry.add.model.DeviceAdditionDto;
 import ru.ylab.example.telemetry.add.mapper.DeviceAdditionMapper;
 import ru.ylab.example.telemetry.add.repository.DeviceAdditionRepository;
 import ru.ylab.example.telemetry.add.service.DeviceAdditionService;
@@ -12,8 +12,8 @@ import ru.ylab.example.telemetry.share.Device;
 @RequiredArgsConstructor
 public class DeviceAdditionServiceImpl implements DeviceAdditionService {
 
-    private final DeviceAdditionRepository deviceAdditionRepository;
     private final DeviceAdditionMapper deviceAdditionMapper;
+    private final DeviceAdditionRepository deviceAdditionRepository;
 
     @Override
     public void addDevice(DeviceAdditionDto request) {
