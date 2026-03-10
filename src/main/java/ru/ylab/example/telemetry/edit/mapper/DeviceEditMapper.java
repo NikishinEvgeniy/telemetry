08354e84517ead0453cb1429158abbfe762a1ccd
NikishinEvgeniy <yevgeniy.nikishin.00@mail.ru>
2026-03-10 19:10:47 +0300

    [T-20] Написал слой DeviceEditService

---
+++ src/main/java/ru/ylab/example/telemetry/edit/mapper/DeviceEditMapper.java
@@ -0,0 +1,13 @@
+package ru.ylab.example.telemetry.edit.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+import ru.ylab.example.telemetry.edit.model.DeviceEditDto;
+import ru.ylab.example.telemetry.share.Device;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.ERROR,
+        componentModel = MappingConstants.ComponentModel.SPRING)
+public interface DeviceEditMapper {
+    Device map(DeviceEditDto deviceEdit);
+}
